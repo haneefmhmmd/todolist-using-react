0653d540e3d6fe223ccab68cae173244b8942df9
haneefmhmmd <haneefmhmmd@gmail.com>
2020-05-09 13:57:01 +0530

    Updates

---
+++ precache-manifest.986c1a01690d881f9954d4d006e91c3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c66506232d67109201b997429da8bc10",
+    "url": "/todolist-using-react/index.html"
+  },
+  {
+    "revision": "afa9bed1887267087f6f",
+    "url": "/todolist-using-react/static/css/main.600f053d.chunk.css"
+  },
+  {
+    "revision": "72f76bbfcb7f6a82baa0",
+    "url": "/todolist-using-react/static/js/2.42b5acfc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todolist-using-react/static/js/2.42b5acfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afa9bed1887267087f6f",
+    "url": "/todolist-using-react/static/js/main.1ef90ec9.chunk.js"
+  },
+  {
+    "revision": "e147420e9e33be6833c1",
+    "url": "/todolist-using-react/static/js/runtime-main.866e3100.js"
+  }
+]);
